54b06f35eff72151e41bb048f47c5faef46a60e7
Maxi Vadora <maxivadora@gmail.com>
2016-05-31 12:21:22 -0300

    scaffold category

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160531141314) do
+ActiveRecord::Schema.define(version: 20160531151817) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
