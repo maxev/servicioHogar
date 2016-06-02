db0316b8f80aee4791aaf80eea2955e9cf715ec5
Maxi Vadora <maxivadora@gmail.com>
2016-06-02 10:46:03 -0300

    Se instalo gem paperclip, se agrego campo avatar a la tabla users, se modifico la edicion del perfil, se creo el metodo permitted params para devise

--- Gemfile
+++ Gemfile
@@ -4,6 +4,10 @@ gem 'devise'
 
 #Validar email
 gem 'valid_email'
+
+#Imagenes con paperclip
+gem "paperclip", git: "git://github.com/thoughtbot/paperclip.git"
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
 # Use sqlite3 as the database for Active Record
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160531154250) do
+ActiveRecord::Schema.define(version: 20160601145824) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -41,6 +41,10 @@ ActiveRecord::Schema.define(version: 20160531154250) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
