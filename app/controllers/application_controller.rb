3e72e725ed754942dc32f18397e39a41935be8b0
Maxi Vadora <maxivadora@gmail.com>
2016-06-03 11:35:15 -0300

    Se creo los checkbox de categorias en la edicion del perfil del usuario

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :get_categories, if: :devise_controller?
+
 
     protected
+    def get_categories
+    	@categories = Category.all
+    end
+
     def configure_permitted_parameters
-        devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:avatar, :current_password) }
+        devise_parameter_sanitizer.for(:account_update) { |u| u.permit(
+        	{category_ids: []}, :avatar , :current_password) }
     end
 end
